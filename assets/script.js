c5ebb2f8e782a031e77b490b9e5a074dcd44768b
ulisesd404 <144632800+ulisesd404@users.noreply.github.com>
2023-11-08 19:32:17 -0800

    almost all of the javascript has been made, debugs are still needed

--- assets/script.js
+++ assets/script.js
@@ -32,14 +32,92 @@ function start() {
 
     document.getElementById("timeLeft").innerHTML = timeLeft;
 
-    timer - setInterval(function() {
+    timer = setInterval(function() {
         timeLeft--;
-        document.getElementById("timeLeft").innerHTML = timeLeft;
+        document.getElementById("timeLeft").innerHTMl = timeLeft;
 
         if (timeLeft <= 0) {
             clearInterval(timer);
             endGame();
         }
+
     }, 1000);
+
     next();
+}
+
+function endGame() {
+    clearInterval(timer);
+
+    var quizContent = `
+    <h2>Game over!</h2>
+    <h3>You got a ` + score + `% </h3>
+    <input type="text" id="name" placeholder="Please enter your initals">
+    <button onclick="setScore()">Set score!</button>`;
+
+    document.getElementById("quizBody").ATTRIBUTE_NODE.innerHTMl = quizContent;
+}
+
+function setScore() {
+    localStorage.setItem("highscore", score);
+    getScore();
+}
+
+function getScore() {
+    var quizContent = localStorage.getItem("highscore") + `%`;
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+function resetGame() {
+    clearInterval(timer);
+    score = 0;
+    currentQuestion = -1;
+    timeLeft = 0
+    timer = null;
+
+    document.getElementById(timeLeft).innerHTML = timeLeft;
+
+    var quizContent = `
+    <h1>
+        Coding Quiz!
+    </h1>
+    <button onclick="start()">Start!</button>`;
+
+    document.getElementById("quizBody").innerHTML = quizContent;
+}
+
+function incorrect() {
+    timeLeft -=15;
+    next();
+}
+
+function correct() {
+    score += 20;
+    next();
+}
+
+function next() {
+    currentQuestion++;
+
+    if (currentQuestion > questions.length - 1) {
+        endGame();
+        return;
+    }
+
+    var quizContent = "<h2>" + questions[currentQuestion].title + "</h2>"
+
+    for (var buttonLoop = 0; buttonLoop < questions[currentQuestion].choices.length; buttonLoop++) {        
+        var buttonCode = "<button onclick=\"[ANS]\">[CHOICE]</button>";         
+        buttonCode = buttonCode.replace("[CHOICE]", questions[currentQuestion].choices[buttonLoop]);        
+        
+        if (questions[currentQuestion].choices[buttonLoop] == questions[currentQuestion].answer) {           
+             buttonCode = buttonCode.replace("[ANS]", "correct()");        
+        }   else { 
+               buttonCode = buttonCode.replace("[ANS]", "incorrect()");       
+             }        
+             quizContent += buttonCode   
+    }
+    
+    document.getElementById("quizBody").innerHTML = quizContent;
 }
